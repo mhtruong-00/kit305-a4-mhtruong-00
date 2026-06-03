82e37e24d4c41f75c623d3fc1cbda28ac49fe18b
mhtruong-00 <mhtruong@utas.edu.au>
2026-06-04 00:22:49 +1000

    Configure platforms: Android minSdk 23 for Firebase, iOS photo-library usage descriptions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
         applicationId = "dev.mhtruong.kit305_a4"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Firebase (firebase_core / cloud_firestore) requires a minimum SDK of 23.
+        minSdk = maxOf(flutter.minSdkVersion, 23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
